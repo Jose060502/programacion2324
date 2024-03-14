7d3fc9727e88d498389cef1fd8e6620f8805adf2
Jose <jseda442@gmail.com>
2024-03-14 14:44:39 +0100

    h

---
+++ Boletin colecciones/src/EjercicioComplementario/Hospital.java
@@ -0,0 +1,29 @@
+package EjercicioComplementario;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Hospital {
+    List<Paciente> pacientes;
+
+    public Hospital() {
+        this.pacientes = new LinkedList<>();
+    }
+
+    public void anadirPaciente(Paciente paciente){
+        Iterator<Paciente> iterator = pacientes.iterator();
+        int posicion = 0;
+        boolean posEncontrada = false;
+
+        while (iterator.hasNext() && !posEncontrada){
+            Paciente paciente1 = iterator.next();
+            if (paciente.compareTo(paciente1) < 0){
+                posEncontrada = true;
+            }else{
+                posicion++;
+            }
+
+        }
+    }
+}
---
+++ Boletin colecciones/src/EjercicioComplementario/Paciente.java
@@ -0,0 +1,55 @@
+package EjercicioComplementario;
+
+import java.util.List;
+
+public class Paciente implements Comparable<Paciente>{
+
+    public static final int MIN_PRIORIDAD = 5;
+    public static final int MAX_PRIORIDAD = 1;
+
+    private String dni;
+    private String nombre;
+    private int edad;
+    private int prioridad;
+
+    private int ordenLlegada;
+
+    public Paciente(String dni, String nombre, int edad, int prioridad, int ordenLlegada) throws HospitalException {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.edad = edad;
+        setPrioridad(prioridad);
+        this.ordenLlegada = ordenLlegada;
+    }
+
+    public int getPrioridad() {
+        return prioridad;
+    }
+
+    public void setPrioridad(int prioridad) throws HospitalException {
+        if (prioridad < MAX_PRIORIDAD || prioridad > MIN_PRIORIDAD){
+            throw new HospitalException("Valor incorrecto");
+        }
+        this.prioridad = prioridad;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    @Override
+    public int compareTo(Paciente otro) {
+        if (prioridad - otro.prioridad == 0){
+            return ordenLlegada - otro.ordenLlegada;
+        }
+        return prioridad - otro.prioridad;
+    }
+}
